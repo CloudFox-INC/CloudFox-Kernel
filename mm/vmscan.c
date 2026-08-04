9146638f95b254cf08022dccb728072bebc2e3e0
CloudFox-INC <CloudFox_INC@proton.me>
2026-08-04 18:19:27 +0000

    mm: Protect the working set under memory pressure (le9uo)
    Import of le9uo working set protection for the Android common kernel
    android12-5.10 branch.  Adds vm.workingset_protection (master
    switch, on by default) and vm.anon_min_ratio / vm.clean_low_ratio /
    vm.clean_min_ratio sysctls that hard- or best-effort-protect
    anonymous and clean file pages from reclaim when their amount drops
    below the configured percentage of node memory.  This prevents
    excessive swap thrashing when memory is overfilled by compressed data
    of the zram module.
    
    Adaptation to android12-5.10:
    - dropped the CONFIG_LRU_GEN isolate_pages() hunks (no MGLRU in 5.10);
    - placed vm_workingset_protection_update_handler() and
      prepare_workingset_protection() after get_scan_count() (5.10 has no
      can_age_anon_pages());
    - called prepare_workingset_protection() at the start of shrink_node()
      (5.10 has no prepare_scan_count());
    - sysctl_workingset_protection is an int with proc_dointvec_minmax
      (0/1) because proc_dobool does not exist in android12-5.10;
    - added the missing <linux/sysinfo.h> include to mm/vmscan.c.
    
    Original patch author: Masahito Suzuki <firelzrd@gmail.com>.
    Source: https://github.com/firelzrd/le9uo
    Link: https://github.com/firelzrd/le9uo#readme
    Signed-off-by: CloudFox-INC <CloudFox_INC@proton.me>
    
    Change-Id: I16fe0385dc379ba578e23f324eeb35cd1d2e2ede

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -45,6 +45,7 @@
 #include <linux/memcontrol.h>
 #include <linux/delayacct.h>
 #include <linux/sysctl.h>
+#include <linux/sysinfo.h>
 #include <linux/oom.h>
 #include <linux/pagevec.h>
 #include <linux/prefetch.h>
@@ -130,6 +131,15 @@ struct scan_control {
 	/* The file pages on the current node are dangerously low */
 	unsigned int file_is_tiny:1;
 
+	/* The anonymous pages on the current node are below vm.anon_min_ratio */
+	unsigned int anon_below_min:1;
+
+	/* The clean file pages on the current node are below vm.clean_low_ratio */
+	unsigned int clean_below_low:1;
+
+	/* The clean file pages on the current node are below vm.clean_min_ratio */
+	unsigned int clean_below_min:1;
+
 	/* Allocation order */
 	s8 order;
 
@@ -180,6 +190,15 @@ struct scan_control {
  * From 0 .. 200.  Higher means more swappy.
  */
 int vm_swappiness = 60;
+
+int sysctl_workingset_protection __read_mostly = 1;
+u8 sysctl_anon_min_ratio  __read_mostly = CONFIG_ANON_MIN_RATIO;
+u8 sysctl_clean_low_ratio __read_mostly = CONFIG_CLEAN_LOW_RATIO;
+u8 sysctl_clean_min_ratio __read_mostly = CONFIG_CLEAN_MIN_RATIO;
+static u64 sysctl_anon_min_ratio_kb  __read_mostly;
+static u64 sysctl_clean_low_ratio_kb __read_mostly;
+static u64 sysctl_clean_min_ratio_kb __read_mostly;
+static u64 workingset_protection_prev_totalram __read_mostly;
 
 #define DEF_KSWAPD_THREADS_PER_NODE 1
 static int kswapd_threads = DEF_KSWAPD_THREADS_PER_NODE;
@@ -2385,6 +2404,15 @@ static void get_scan_count(struct lruvec *lruvec, struct scan_control *sc,
 	trace_android_rvh_set_balance_anon_file_reclaim(&balance_anon_file_reclaim);
 
 	/*
+	 * Force-scan anon if clean file pages is under vm.clean_low_ratio
+	 * or vm.clean_min_ratio.
+	 */
+	if (sc->clean_below_low || sc->clean_below_min) {
+		scan_balance = SCAN_ANON;
+		goto out;
+	}
+
+	/*
 	 * If there is enough inactive page cache, we do not reclaim
 	 * anything from the anonymous working right now. But when balancing
 	 * anon and page cache files for reclaim, allow swapping of anon pages
@@ -2531,7 +2559,97 @@ out:
 			BUG();
 		}
 
+		/*
+		 * Hard protection of the working set.
+		 * Don't reclaim anon/file pages when the amount is
+		 * below the watermark of the same type.
+		 */
+		if (file ? sc->clean_below_min : sc->anon_below_min)
+			scan = 0;
+
 		nr[lru] = scan;
+	}
+}
+
+int vm_workingset_protection_update_handler(struct ctl_table *table, int write,
+					     void *buffer, size_t *lenp,
+					     loff_t *ppos)
+{
+	int ret = proc_dou8vec_minmax(table, write, buffer, lenp, ppos);
+
+	if (ret || !write)
+		return ret;
+
+	workingset_protection_prev_totalram = 0;
+
+	return 0;
+}
+
+static void prepare_workingset_protection(pg_data_t *pgdat, struct scan_control *sc)
+{
+	unsigned long node_mem_total;
+	struct sysinfo i;
+
+	if (!sysctl_workingset_protection) {
+		sc->anon_below_min = 0;
+		sc->clean_below_low = 0;
+		sc->clean_below_min = 0;
+		return;
+	}
+
+	if (likely(sysctl_anon_min_ratio ||
+		   sysctl_clean_low_ratio ||
+		   sysctl_clean_min_ratio)) {
+#ifdef CONFIG_NUMA
+		si_meminfo_node(&i, pgdat->node_id);
+#else
+		si_meminfo(&i);
+#endif
+		node_mem_total = i.totalram;
+
+		if (unlikely(workingset_protection_prev_totalram != node_mem_total)) {
+			sysctl_anon_min_ratio_kb =
+				node_mem_total * sysctl_anon_min_ratio / 100;
+			sysctl_clean_low_ratio_kb =
+				node_mem_total * sysctl_clean_low_ratio / 100;
+			sysctl_clean_min_ratio_kb =
+				node_mem_total * sysctl_clean_min_ratio / 100;
+			workingset_protection_prev_totalram = node_mem_total;
+		}
+	}
+
+	if (sysctl_anon_min_ratio) {
+		unsigned long reclaimable_anon;
+
+		reclaimable_anon =
+			node_page_state(pgdat, NR_ACTIVE_ANON) +
+			node_page_state(pgdat, NR_INACTIVE_ANON) +
+			node_page_state(pgdat, NR_ISOLATED_ANON);
+
+		sc->anon_below_min = reclaimable_anon < sysctl_anon_min_ratio_kb;
+	} else {
+		sc->anon_below_min = 0;
+	}
+
+	if (sysctl_clean_low_ratio || sysctl_clean_min_ratio) {
+		unsigned long reclaimable_file, dirty, clean;
+
+		reclaimable_file =
+			node_page_state(pgdat, NR_ACTIVE_FILE) +
+			node_page_state(pgdat, NR_INACTIVE_FILE) +
+			node_page_state(pgdat, NR_ISOLATED_FILE);
+		dirty = node_page_state(pgdat, NR_FILE_DIRTY);
+
+		if (likely(reclaimable_file > dirty))
+			clean = reclaimable_file - dirty;
+		else
+			clean = 0;
+
+		sc->clean_below_low = clean < sysctl_clean_low_ratio_kb;
+		sc->clean_below_min = clean < sysctl_clean_min_ratio_kb;
+	} else {
+		sc->clean_below_low = 0;
+		sc->clean_below_min = 0;
 	}
 }
 
@@ -2789,6 +2907,8 @@ static void shrink_node(pg_data_t *pgdat, struct scan_control *sc)
 	unsigned long file;
 
 	target_lruvec = mem_cgroup_lruvec(sc->target_mem_cgroup, pgdat);
+
+	prepare_workingset_protection(pgdat, sc);
 
 again:
 	memset(&sc->nr, 0, sizeof(sc->nr));
